a5834ff9dc0f6bb5eba2af3e4528756c925164f1
leeyongwon <33643585+LeeYongWon@users.noreply.github.com>
2022-12-13 22:29:48 +0900

    router Home 컴포넌트 생성

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,56 +1,10 @@
-import { useEffect, useState } from "react";
-
+import Home from './routes/Home'
 const App = () => {
-  const [movieArr, setMovieArr] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const getMoive = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.3&sort_by=year"
-    );
-    const json = await response.json();
-    setMovieArr(json.data.movies);
-    setLoading(false);
-    console.log(json.data.movies);
-  };
-  useEffect(() => {
-    /**
-then을 사용하기보다는 async-await를 사용해야한다
-fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
-    .then((res) => res.json())
-    .then((json) => {
-    setMovieArr(json.data.movies);
-    setLoading(false);
-    });
-*/
-    getMoive();
-  }, []);
-
-  return (
+  return(
     <div>
-      <ul>
-        {loading ? (
-          <strong>Loading....</strong>
-        ) : (
-          movieArr.map((movie) => {
-            return (
-              <div key={movie.id}>
-                <img src={movie.medium_cover_image} alt="" />
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((genre, idx) => (
-                    <li key={idx}>{genre}</li>
-                  ))}
-                </ul>
-                <hr/>
-              </div>
-            );
-          })
-        )}
-      </ul>
+      <Home />
     </div>
-  );
+  )
 };
 
 export default App;
---
+++ movie-app/src/routes/Home.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import Movie from '../component/Movie';
+const Home = () =>{
+    const [movieArr, setMovieArr] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+  
+    const getMoive = async () => {
+      const response = await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      );
+      const json = await response.json();
+      setMovieArr(json.data.movies);
+      setLoading(false);
+      console.log(json.data.movies);
+    };
+    useEffect(() => {
+      /**
+  then을 사용하기보다는 async-await를 사용해야한다
+  fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+      .then((res) => res.json())
+      .then((json) => {
+      setMovieArr(json.data.movies);
+      setLoading(false);
+      });
+  */
+      getMoive();
+    }, []);
+  
+    return (
+      <div>
+        <ul>
+          {loading ? (
+            <strong>Loading....</strong>
+          ) : (
+            <div>
+              {movieArr.map((movie) => {
+                return(
+                <Movie
+                  key={movie.id}
+                  
+                  coverImg={movie.medium_cover_image}
+                  title={movie.title}
+                  summary={movie.summary}
+                  genres={movie.genres}
+                />)
+              })}
+            </div>
+          )}
+        </ul>
+      </div>
+    );
+
+}
+
+export default Home
