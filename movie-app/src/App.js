9a4034972d1c7df505800842c28894cdd7e0cefb
leeyongwon <33643585+LeeYongWon@users.noreply.github.com>
2022-12-13 20:41:28 +0900

    fetch로 api데이터 가져와서 render해주기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,15 +4,17 @@ const App = () => {
   const [movieArr, setMovieArr] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const getMoive = async ()=>{
-    const response =await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
+  const getMoive = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.3&sort_by=year"
+    );
     const json = await response.json();
-    setMovieArr(json.data.movies)
+    setMovieArr(json.data.movies);
     setLoading(false);
-
-  }
+    console.log(json.data.movies);
+  };
   useEffect(() => {
-/**
+    /**
 then을 사용하기보다는 async-await를 사용해야한다
 fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
     .then((res) => res.json())
@@ -21,11 +23,7 @@ fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
     setLoading(false);
     });
 */
-getMoive()
-
-
-
-
+    getMoive();
   }, []);
 
   return (
@@ -34,7 +32,21 @@ getMoive()
         {loading ? (
           <strong>Loading....</strong>
         ) : (
-          movieArr.map((el) => <li key={el.id}>{el.title}</li>)
+          movieArr.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <img src={movie.medium_cover_image} alt="" />
+                <h2>{movie.title}</h2>
+                <p>{movie.summary}</p>
+                <ul>
+                  {movie.genres.map((genre, idx) => (
+                    <li key={idx}>{genre}</li>
+                  ))}
+                </ul>
+                <hr/>
+              </div>
+            );
+          })
         )}
       </ul>
     </div>
